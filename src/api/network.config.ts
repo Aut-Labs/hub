6beee8db8437e90bd52c88f406b893ac00047728
jabyl <jabyl.new@gmail.com>
2026-01-09 12:26:59 +0100

    updated deps

--- src/api/network.config.ts
+++ src/api/network.config.ts
@@ -16,7 +16,6 @@ export interface NetworkConfig {
   chainId: string | number;
   rpcUrls: string[];
   explorerUrls: string[];
-  biconomyApiKey: string;
   contracts: NetworkContracts;
   disabled: boolean;
   nativeCurrency: any;
